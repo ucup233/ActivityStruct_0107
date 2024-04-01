c17ea7a50f778f9bf083eb178590daea53f854ce
Yusuf Hidayat <sufhidayat9@gmail.com>
2024-04-01 16:18:57 +0700

    input informasi sub struct detail alamat

--- StructinStruct/StructinStruct.cpp
+++ StructinStruct/StructinStruct.cpp
@@ -19,6 +19,13 @@ int main() {
 	cin >> mhs.nim;
 	cout << "Nama Mahasiswa : ";
 	cin >> mhs.nama;
+
+	cout << "Alamat Mahasiswa : ";
+	cout << "\t Nama Desa : ";
+	cin >> mhs.alamat.desa;
+	cout << "\t Nama Kota : ";
+	cin >> mhs.alamat.kota;
+
 	cout << "Umur Mahasiswa : ";
 	cin >> mhs.umur;
 		
