76fc2eefae3307dc40d57e99f229f76251e7a21a
Yusuf Hidayat <sufhidayat9@gmail.com>
2024-04-01 16:57:18 +0700

    input informasi strcut mahasiswa

--- getlineStruct/getlineStruct.cpp
+++ getlineStruct/getlineStruct.cpp
@@ -21,5 +21,14 @@ int main() {
 	cout << "Nama Mahasiswa : ";
 	getline(cin, mhs.nama);
 
+	cout << "Alamat Mahasiswa : " << endl;
+	cout << "\t Nama Desa : ";
+	cin >> mhs.alamat.desa;
+	cout << "\t Nama Kota : ";
+	cin >> mhs.alamat.kota;
 
+	cout << "Umur Mahasiswa : ";
+	cin >> mhs.umur;
+
+	
 }
